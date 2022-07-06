5c6cc79dd84db334883adfcf09d4bf6515550766
Carlos Augusto D. Pacheco <carlos.pacheco@aluno.ifsp.edu.br>
2022-07-05 22:24:10 -0300

    finalizacao modulo Contador

---
+++ Contador/assets/js/scripts.js
@@ -0,0 +1,20 @@
+var currentNumberWrapper = document.getElementById("currentNumber")
+var currentNumber = 0;
+
+function increment() {
+    if (currentNumber <10) {
+    currentNumber = currentNumber + 1;
+    currentNumberWrapper.innerHTML = currentNumber;
+    } else {
+    currentNumberWrapper.innerHTML = currentNumber;
+    }
+}
+
+function decrement() {
+    if(currentNumber <=0) {
+        currentNumberWrapper.innerHTML = currentNumber;
+    } else {
+    currentNumber = currentNumber - 1;
+    currentNumberWrapper.innerHTML = currentNumber;
+    }
+}
