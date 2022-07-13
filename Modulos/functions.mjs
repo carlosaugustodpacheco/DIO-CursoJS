7bfb2304fd3915d73b456b3836569b7a6b12a360
Carlos Augusto D. Pacheco <carlos.pacheco@aluno.ifsp.edu.br>
2022-07-12 23:56:06 -0300

    finalizacao do capitulo de MODULOS.

---
+++ Modulos/functions.mjs
@@ -0,0 +1,17 @@
+function mostraIdade(nome, idade) {
+    return `A idade do ${nome} é ${idade}`;
+}
+
+function mostraCidade(nome, cidade) {
+    return `A cidade do ${nome} é ${cidade}`;
+}
+
+function mostraHobby(nome, hobby) {
+    return `A cidade do ${nome} é ${hobby}`;
+}
+
+export {
+    mostraCidade,
+    mostraHobby,
+    mostraIdade
+};
---
+++ Modulos/main.mjs
@@ -0,0 +1,5 @@
+import { mostraCidade, mostraIdade, mostraHobby } from "./functions.mjs";
+
+console.log(mostraIdade('Guto', 28));
+console.log(mostraCidade('Gutinho', 'São Paulo'));
+console.log(mostraHobby('Guto', 'ser maromba'));
